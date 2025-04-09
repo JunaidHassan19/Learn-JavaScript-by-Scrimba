f83a6a74992aba1df9ccd992b281e831c14c5f77
JunaidHassan19 <thejunni01@gmail.com>
2025-04-09 14:12:51 +0530

    Write your first function

--- 01 Build a Passenger Counter App/index.js
+++ 01 Build a Passenger Counter App/index.js
@@ -1,17 +1,8 @@
-function countDown() {
-  console.log(5);
-  console.log(4);
-  console.log(3);
-  console.log(2);
-  console.log(1);
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+function fun() {
+  console.log(24);
 }
 
-// Setting up the the race 🏎 🏎 🏎
-countDown();
-
-// GO! 🏁
-// Players are running the race 🏎 💨
-// Race is finished! 🍾
-
-// Get ready for a new race 🏎 🏎 🏎
-countDown();
+fun();
