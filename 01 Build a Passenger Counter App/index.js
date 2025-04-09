ce7b3351233f4faf334611f86b43ebb381887d82
JunaidHassan19 <thejunni01@gmail.com>
2025-04-09 18:31:14 +0530

    Write your first string variable

--- 01 Build a Passenger Counter App/index.js
+++ 01 Build a Passenger Counter App/index.js
@@ -1,14 +1,11 @@
-// grab the count-el element, store it in a countEl variable
-const countEl = document.getElementById("count-el");
-let count = 0;
+let userName = "John Doe";
 
-function increment() {
-  count = count + 1;
-  // set countEl's innerText to the count
-  countEl.innerText = count;
-}
+console.log(userName);
 
-// 1. Create a function, save(), which logs out the count when it's called
-function save() {
-  console.log(count);
-}
+// Create a variable, message, that stores the string: "You have tree new notifications"
+let message = "You have three new notifications";
+console.log(message);
+
+// Create a variable, messageToUser, that contains the message we have logged
+let messageToUser = message + ", " + userName;
+console.log(messageToUser);
