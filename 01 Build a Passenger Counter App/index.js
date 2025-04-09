617ec6336549d9c6e1149727e230b711380719a7
JunaidHassan19 <thejunni01@gmail.com>
2025-04-09 13:10:19 +0530

    Basic mathematical operations

--- 01 Build a Passenger Counter App/index.js
+++ 01 Build a Passenger Counter App/index.js
@@ -1,11 +1,19 @@
 // document.getElementById("count").innerText = 5;
 
-let count = 0;
+/*
+// let count = 5 * 7;
+let firstBatch = 5;
+let secondBatch = 7;
+let count = firstBatch + secondBatch;
 
 console.log(count);
+*/
 
-// 1. Create a variable, myAge, and set its value to your age
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log myDogAge to the console
+
 let myAge = 19;
-
-// 2. Log the myAge variable to the console
-console.log(myAge);
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);
