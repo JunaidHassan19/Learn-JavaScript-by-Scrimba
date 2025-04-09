e51206b5d7e6fb863a809044018a32abd7f6933d
JunaidHassan19 <thejunni01@gmail.com>
2025-04-09 14:50:02 +0530

    Display the count

--- 01 Build a Passenger Counter App/index.js
+++ 01 Build a Passenger Counter App/index.js
@@ -1,16 +1,11 @@
-// document.getElementById("count").innerText = 5
+// document.getElementById("count-el").innerText = 5
 
-// let count = 0;
-// count = count + 1;
-// console.log(count);
-
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
 let count = 0;
+let countEl = document.getElementById("count-el");
+
 function increment() {
   count += 1;
-  console.log(count);
+  countEl.innerText = count;
 }
