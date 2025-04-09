19d47b36d1382222348d420a08f5de9542eeee85
JunaidHassan19 <thejunni01@gmail.com>
2025-04-09 18:58:08 +0530

    Improve the message with string concatenation

--- 01 Build a Passenger Counter App/index.js
+++ 01 Build a Passenger Counter App/index.js
@@ -1,10 +1,12 @@
-// Grab the welcome-el paragraph and store it in a variable called welcomeEl
-const welcomeEl = document.getElementById("welcome-el");
+let welcomeEl = document.getElementById("welcome-el");
 
-// Create two variables (name & greeting) that contains your name
-// and the greeting we want to render on the page
-let name = "junni";
-let greeting = "Hi, my name is ";
+let name = "Junior";
+let greeting = "Welcome back ";
 
-// Render the welcome message using welcomeEl.innerText
-welcomeEl.innerText = `${greeting}${name}`;
+welcomeEl.innerText = greeting + name;
+
+// Add an emoji to the end! 👋
+// WRITE YOUR CODE BELOW HERE
+// HINT: count = count + 1
+
+welcomeEl.innerText += "👋";
