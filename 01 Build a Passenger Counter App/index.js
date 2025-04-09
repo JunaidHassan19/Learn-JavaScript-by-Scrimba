e962b0e88a2dccfa6c473f39d0abedd09b500fb7
JunaidHassan19 <thejunni01@gmail.com>
2025-04-09 13:36:09 +0530

    The onclick event listener

--- 01 Build a Passenger Counter App/index.js
+++ 01 Build a Passenger Counter App/index.js
@@ -1,25 +1,10 @@
 // document.getElementById("count").innerText = 5;
 
-/*
-let count = 5;
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
 
-// count = 4;  output: 4, not 5 because we re-assigned the value of count to 4
-count = count - 1; // decrement the value of count by 1
-count = count + 1; // increment the value of count by 1
-count = 0; // reset the value of count to 0
-
-console.log(count);
-*/
-
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
-
-let bonusPoints = 50; // Initialize bonusPoints to 50
-console.log(bonusPoints); // Output: 50
-bonusPoints += 50; // Increase bonusPoints by 50
-console.log(bonusPoints); // Output: 100
-bonusPoints -= 75; // Decrease bonusPoints by 75
-console.log(bonusPoints); // Output: 25
-bonusPoints += 45; // Increase bonusPoints by 45
-console.log(bonusPoints); // Output: 70
+function increment() {
+  console.log("Increment button clicked");
+}
