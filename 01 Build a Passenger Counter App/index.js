cc1b117355c90e1b8391a9b9d5672625006bc330
JunaidHassan19 <thejunni01@gmail.com>
2025-04-09 13:21:15 +0530

    Reassigning and incrementing

--- 01 Build a Passenger Counter App/index.js
+++ 01 Build a Passenger Counter App/index.js
@@ -1,19 +1,25 @@
 // document.getElementById("count").innerText = 5;
 
 /*
-// let count = 5 * 7;
-let firstBatch = 5;
-let secondBatch = 7;
-let count = firstBatch + secondBatch;
+let count = 5;
+
+// count = 4;  output: 4, not 5 because we re-assigned the value of count to 4
+count = count - 1; // decrement the value of count by 1
+count = count + 1; // increment the value of count by 1
+count = 0; // reset the value of count to 0
 
 console.log(count);
 */
 
-// 1. Create two variables, myAge and humanDogRatio
-// 2. Multiply the two together and store the result in myDogAge
-// 3. Log myDogAge to the console
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
 
-let myAge = 19;
-let humanDogRatio = 7;
-let myDogAge = myAge * humanDogRatio;
-console.log(myDogAge);
+let bonusPoints = 50; // Initialize bonusPoints to 50
+console.log(bonusPoints); // Output: 50
+bonusPoints += 50; // Increase bonusPoints by 50
+console.log(bonusPoints); // Output: 100
+bonusPoints -= 75; // Decrease bonusPoints by 75
+console.log(bonusPoints); // Output: 25
+bonusPoints += 45; // Increase bonusPoints by 45
+console.log(bonusPoints); // Output: 70
