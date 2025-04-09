9e0bee5bdd28dcb5a45c97a76a4464c44cd99fcc
JunaidHassan19 <thejunni01@gmail.com>
2025-04-09 14:39:19 +0530

    Increment on clicks

--- 01 Build a Passenger Counter App/index.js
+++ 01 Build a Passenger Counter App/index.js
@@ -1,15 +1,16 @@
-let lapsCompleted = 0;
+// document.getElementById("count").innerText = 5
 
-// Create a function that increments the lapsCompleted variable with one
-// Run it three times
+// let count = 0;
+// count = count + 1;
+// console.log(count);
 
-function incrementLaps() {
-  lapsCompleted += 1;
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+let count = 0;
+function increment() {
+  count += 1;
+  console.log(count);
 }
-incrementLaps();
-incrementLaps();
-incrementLaps();
-
-// Print it to the console
-
-console.log(lapsCompleted);
