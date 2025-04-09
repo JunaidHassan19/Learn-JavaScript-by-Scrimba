d4a74c50d963d51ada4c4ed4734556af2014fa90
JunaidHassan19 <thejunni01@gmail.com>
2025-04-09 18:40:38 +0530

    Strings vs. Numbers

--- 01 Build a Passenger Counter App/index.js
+++ 01 Build a Passenger Counter App/index.js
@@ -1,6 +1,14 @@
-// Create two variables, name and greeting. The name variable should store your name,
-// and the greeting should store e.g. "Hi, my name is "
-let name = "John Doe";
+let name = 42;
 let greeting = "Hi, my name is ";
+let myGreeting = greeting + name;
+// console.log(myGreeting)
 
-console.log(`${greeting}${name}`);
+// let points = 4
+// let bonusPoints = "10"
+
+// let totalPoints = points + bonusPoints
+
+console.log(4 + 5); //  9
+console.log("2" + "4"); //24
+console.log("5" + 1); // 51
+console.log(100 + "100"); // 100100
