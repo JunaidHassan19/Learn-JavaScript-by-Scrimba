035050f717730154f3a90ecd55d4b68da1e432db
JunaidHassan19 <thejunni01@gmail.com>
2025-04-10 11:15:10 +0530

    Concatenate two strings in a function

---
+++ 02 Practice Time - Part 1/2.2-concatenate-strings.js
@@ -0,0 +1,9 @@
+let name = "Linda";
+let greeting = "Hi there";
+
+// Create a function that logs out "Hi there, Linda!" when called
+function greet() {
+  console.log(greeting + ", " + name + "!"); // Concatenating the greeting and name variables
+}
+
+greet(); // Calling the function to log the greeting
