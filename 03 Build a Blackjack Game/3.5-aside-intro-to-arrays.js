b9f0ff3b691c049fa8fd820b4631a066532d1b80
JunaidHassan19 <thejunni01@gmail.com>
2025-04-11 14:36:49 +0530

    Arrays with multiple data types

--- 03 Build a Blackjack Game/3.5-aside-intro-to-arrays.js
+++ 03 Build a Blackjack Game/3.5-aside-intro-to-arrays.js
@@ -1,17 +1,32 @@
-// Arrays - ordered lists of items - coposite / complex data type
+// Arrays - ordered lists of items - composite / complex data type
 
-// 0 indexed
-// Arrays can hold any type of data
+// Arrays in JavaScript:
+// - Are 0-indexed (first item is at position 0, second at position 1, etc.)
+// - Can hold any type of data (strings, numbers, booleans, objects, other arrays)
+// - Are created using square brackets []
+// - Allow storing multiple values in a single variable
 
+// Creating an array of strings with three blog post titles
 let featuredPosts = [
-  "Check out my Netflix clone",
-  "Here's the code for my project",
-  "I've just relaunched my portfolio",
+  "Check out my Netflix clone", // Position 0
+  "Here's the code for my project", // Position 1
+  "I've just relaunched my portfolio", // Position 2
 ];
-console.log(featuredPosts[0]); // Check out my Netflix clone
-console.log(featuredPosts[1]); // Here's the code for my project
 
-// Create an array that describes yourself. Use the three primitive data types you've learned
-// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+// Accessing array elements using their index position inside square brackets
+console.log(featuredPosts[0]); // Output: "Check out my Netflix clone"
+console.log(featuredPosts[1]); // Output: "Here's the code for my project"
+
+// Exercise: Create an array that describes yourself using three primitive data types
+// The array contains:
+// - A string (name)
+// - A number (age)
+// - A boolean (whether you like pizza or not)
 
 let myInfo = ["Junni", 30, true];
+// myInfo[0] is the string "Junni"
+// myInfo[1] is the number 30
+// myInfo[2] is the boolean true (indicating "likes pizza")
+
+// Note: Arrays can mix different data types like this example shows
+// Arrays are very flexible and are one of the most commonly used data structures in JavaScript
