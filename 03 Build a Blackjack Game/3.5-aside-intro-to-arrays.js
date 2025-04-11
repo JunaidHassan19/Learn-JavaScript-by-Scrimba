b12a6b3387b8343e21b67a226bd178e13a7f9be7
JunaidHassan19 <thejunni01@gmail.com>
2025-04-11 14:23:25 +0530

    Aside: Intro to arrays

---
+++ 03 Build a Blackjack Game/3.5-aside-intro-to-arrays.js
@@ -0,0 +1,18 @@
+// Arrays - ordered lists of items
+
+let featuredPosts = [
+  "Check out my Netflix clone",
+  "Here's the code for my project",
+  "I've just relaunched my portfolio",
+];
+
+// Create an array that lists your i.e. experience, education, licenses, skills or similar
+// The items of the array should be strings
+
+let profile = [
+  "Full-Stack Developer",
+  "Software Engineer",
+  "Computer Science Graduate",
+  "Certified Scrum Master",
+  "React.js, Node.js, Python, Java",
+];
