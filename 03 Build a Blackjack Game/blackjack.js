7d784a9759e19b7b975289f4c40b247281803d36
JunaidHassan19 <thejunni01@gmail.com>
2025-04-11 10:35:33 +0530

    Add the isAlive variable

--- 03 Build a Blackjack Game/blackjack.js
+++ 03 Build a Blackjack Game/blackjack.js
@@ -2,15 +2,19 @@ let firstCard = 10;
 let secondCard = 7;
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
-
+// 1. Create a variable called isAlive and assign it to true
+let isAlive = true;
+// 2. Flip its value to false in the appropriate code block
 if (sum <= 20) {
   console.log("Do you want to draw a new card? 🙂");
+  isAlive = true;
 } else if (sum === 21) {
   console.log("Wohoo! You've got Blackjack! 🥳");
   hasBlackjack = true;
 } else {
   console.log("You're out of the game! 😭");
+  isAlive = false;
 }
 
-// CARD COUNTING
-console.log(hasBlackjack);
+// 3. Log it out to check that you're doing it right
+console.log(isAlive);
