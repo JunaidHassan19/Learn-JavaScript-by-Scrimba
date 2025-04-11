7bd1d1ca44bab73bd89be0ce20abbf245c8f64dd
JunaidHassan19 <thejunni01@gmail.com>
2025-04-11 14:09:47 +0530

    Rename the startGame function

--- 03 Build a Blackjack Game/blackjack.js
+++ 03 Build a Blackjack Game/blackjack.js
@@ -6,11 +6,15 @@ let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
-// 2. Store the cards paragraph in a variable called cardsEl
 let cardsEl = document.getElementById("cards-el");
 
+// Create a new function called startGame() that calls renderGame()
+
 function startGame() {
-  // 3. Render the cards on the page using this format -> "Cards: 10 4"
+  renderGame();
+}
+
+function renderGame() {
   cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
@@ -29,11 +33,7 @@ function startGame() {
 
 function newCard() {
   console.log("Drawing a new card from the deck!");
-  // 1. Create a card variable, and hard code its value to a number (2-11)
-  let card = 5; // Example hard-coded card value
-  // 2. Add the new card to the sum variable
+  let card = 5;
   sum += card;
-
-  // 3. Call startGame()
-  startGame();
+  renderGame();
 }
