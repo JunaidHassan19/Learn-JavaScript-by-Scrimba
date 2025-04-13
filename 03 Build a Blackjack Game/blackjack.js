920bb1e239256504a2e033e025740e3b066e9aa3
JunaidHassan19 <thejunni01@gmail.com>
2025-04-13 11:34:10 +0530

    Only trigger newCard() if you're allowed to

--- 03 Build a Blackjack Game/blackjack.js
+++ 03 Build a Blackjack Game/blackjack.js
@@ -51,7 +51,6 @@ function renderGame() {
 function newCard() {
   // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
   if (isAlive === true && hasBlackjack === false) {
-    return;
     let card = getRandomCard();
     sum += card;
     cards.push(card);
