c4df37f27d862ef316ed1bd890e778f2b45c1f33
JunaidHassan19 <thejunni01@gmail.com>
2025-04-13 17:13:23 +0530

    Solo Project (PRO) - Password Generator

---
+++ 04 Objects and functions/index.js
@@ -0,0 +1,114 @@
+let displayOne = document.getElementById("password-display-1");
+let displayTwo = document.getElementById("password-display-2");
+let passwordButton = document.getElementById("generate-password");
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+let passwordLength = 13;
+
+function generateRandomPassword() {
+  let password = "";
+  for (i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+  return password;
+}
+
+passwordButton.addEventListener("click", function () {
+  const passwordOne = generateRandomPassword();
+  const passwordTwo = generateRandomPassword();
+
+  displayOne.textContent = passwordOne;
+  displayTwo.textContent = passwordTwo;
+});
