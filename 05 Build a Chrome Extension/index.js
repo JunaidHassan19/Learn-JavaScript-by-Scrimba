1d649351f29d4388054be4649107f28b3494b0e4
JunaidHassan19 <thejunni01@gmail.com>
2025-04-18 12:19:33 +0530

    Improving the performance of our app

--- 05 Build a Chrome Extension/index.js
+++ 05 Build a Chrome Extension/index.js
@@ -2,6 +2,7 @@ let myLeads = ["www.google.com", "www.facebook.com", "www.instagram.com"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+let listItems = "";
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
@@ -9,13 +10,11 @@ inputBtn.addEventListener("click", function () {
   console.log(myLeads);
 });
 
-// Let's try a different method!
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
 for (let i = 0; i < myLeads.length; i++) {
-  // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-  // create element
-  // set text content
-  // append to ul
-  const li = document.createElement("li");
-  li.textContent = myLeads[i];
-  ulEl.append(li);
+  // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+  listItems += "<li>" + myLeads[i] + "</li>";
 }
+// 3. Render the listItems inside the unordered list using ulEl.innerHTML
+ulEl.innerHTML = listItems;
