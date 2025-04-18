b312831df5a95c1edf98b2a7c6e7f310be244e44
JunaidHassan19 <thejunni01@gmail.com>
2025-04-18 13:00:53 +0530

    Clear the input field

--- 05 Build a Chrome Extension/index.js
+++ 05 Build a Chrome Extension/index.js
@@ -6,15 +6,20 @@ const ulEl = document.getElementById("ul-el");
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
-  // 2. Call the renderLeads() function
   renderLeads();
 });
 
-// 1. Wrap the code below in a renderLeads() function
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    // Wrap the lead in an anchor tag (<a>) inside the <li>
+    // Can you make the link open in a new tab?
+    listItems +=
+      "<li><a target='_blank' href='" +
+      myLeads[i] +
+      "'>" +
+      myLeads[i] +
+      "</a></li>";
   }
   ulEl.innerHTML = listItems;
 }
