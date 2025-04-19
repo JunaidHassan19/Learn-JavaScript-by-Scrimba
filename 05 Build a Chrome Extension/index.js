d9747416cefc4ac5dc31fbea0d2a5430a3f861f3
JunaidHassan19 <thejunni01@gmail.com>
2025-04-19 20:16:44 +0530

    Storing arrays in localStorage

--- 05 Build a Chrome Extension/index.js
+++ 05 Build a Chrome Extension/index.js
@@ -1,20 +1,17 @@
-let myLeads = [];
+let myLeads = `["https://www.google.com", "https://www.facebook.com", "https://www.instagram.com"]`;
+
+// 1. Turn the myLeads string into an array
+myLeads = JSON.parse(myLeads);
+// 2. Push a new value to the array
+myLeads.push("https://www.twitter.com");
+// 3. Turn the array into a string again
+myLeads = JSON.stringify(myLeads);
+// 4. Console.log the string using typeof to verify that it's a string
+console.log(typeof myLeads);
+
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-
-// 1. Save a key-value pair in localStorage
-localStorage.setItem("myName", "Junni");
-// 2. Refresh the page. Get the value and log it to the console
-console.log(localStorage.getItem("myName"));
-// 3. Clear localStorage
-localStorage.clear();
-
-// HINTS:
-// localStorage.setItem(key, value)
-// localStorage.getItem(key)
-// localStorage.clear()
-// PS: both key and value need to be strings
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
@@ -25,12 +22,6 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    // listItems +=
-    //   "<li><a target='_blank' href='" +
-    //   myLeads[i] +
-    //   "'>" +
-    //   myLeads[i] +
-    //   "</a></li>";
     listItems += `
       <li>
         <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
