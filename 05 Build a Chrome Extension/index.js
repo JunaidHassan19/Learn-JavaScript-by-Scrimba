386f4b859b6d1870b2f552af1cf71bb03f3f44f0
JunaidHassan19 <thejunni01@gmail.com>
2025-04-20 12:49:50 +0530

    Checking localStorage before rendering

--- 05 Build a Chrome Extension/index.js
+++ 05 Build a Chrome Extension/index.js
@@ -3,11 +3,15 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
-// Get the leads from the localStorage - PS: JSON.parse()
-// Store it in a variable, leadsFromLocalStorage
-// Log out the variable
+// ["lead1", "lead2"] or null
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage);
+
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
@@ -16,7 +20,6 @@ inputBtn.addEventListener("click", function () {
 
   renderLeads();
 
-  // To verify that it works:
   console.log(localStorage.getItem("myLeads"));
 });
 
