84990fdef070b6111d9dda7115c4e43475fc48ca
JunaidHassan19 <thejunni01@gmail.com>
2025-04-20 12:42:59 +0530

    Guess the expression - truthy or falsy

---
+++ 05 Build a Chrome Extension/5.6-guess-expression.js
@@ -0,0 +1,13 @@
+let trueOrFalse = Boolean("Hello");
+
+console.log(trueOrFalse); // true
+console.log(Boolean("")); // false
+
+console.log(Boolean("")); // false
+console.log(Boolean("0")); // true
+console.log(Boolean(100)); // true
+console.log(Boolean(null)); // false
+console.log(Boolean([0])); // true
+console.log(Boolean(-0)); // false
+console.log(Boolean(" ")); // true
+console.log(Boolean("false")); // true
