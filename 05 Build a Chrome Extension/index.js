8ae37e083e2c7c0ce44f346d224b73dc836c8b54
JunaidHassan19 <thejunni01@gmail.com>
2025-04-21 12:06:17 +0530

    Save the tab url

--- 05 Build a Chrome Extension/index.js
+++ 05 Build a Chrome Extension/index.js
@@ -4,7 +4,6 @@ const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-// 1. Grab the SAVE TAB button and store it in a tabBtn variable
 const tabBtn = document.getElementById("tab-btn");
 
 if (leadsFromLocalStorage) {
@@ -14,9 +13,11 @@ if (leadsFromLocalStorage) {
 
 const tabs = [{ url: "https://www.linkedin.com/in/junaidhassan19/" }];
 
-// 2. Listen for clicks on tabBtn. Log Per's LinkedIn URL to the console
 tabBtn.addEventListener("click", function () {
-  console.log(tabs[0].url);
+  // Save the url instead of logging it out
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
 
 function render(leads) {
