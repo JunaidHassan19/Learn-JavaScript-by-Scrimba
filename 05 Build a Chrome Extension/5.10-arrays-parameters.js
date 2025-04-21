235dadccf21761a0664ce841d2dfbf591d3c0b31
JunaidHassan19 <thejunni01@gmail.com>
2025-04-21 11:40:28 +0530

    Arrays as parameters

---
+++ 05 Build a Chrome Extension/5.10-arrays-parameters.js
@@ -0,0 +1,7 @@
+// Create a function, getFirst(arr), that returns the first item in the array
+
+// Call it with an array as an argument to verify that it works
+function getFirst(arr) {
+  return arr[0];
+}
+console.log(getFirst(["apple", "banana", "cherry"])); // Output: "apple"
